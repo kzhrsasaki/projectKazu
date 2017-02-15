1794a3316327f67a63ba1b6a2fef1b8296600d2f
kzhrsasaki <kzhr.sasaki@gmail.com>
2017-02-15 12:51:35 +0900

    2/15クラス終了分

--- projectKazu/ViewController.swift
+++ projectKazu/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    @IBOutlet weak var enterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func changeSwitch(_ sender: UISwitch) {
+        print(sender.isOn)
+        
+        if sender.isOn {
+            print("スイッチON")
+        }else{
+            print("スイッチOFF")
+        }
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ projectKazu/secondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  secondViewController.swift
+//  projectKazu
+//
+//  Created by Apple on 2017/02/15.
+//  Copyright © 2017年 Sasaki Kazuhiro. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    @IBOutlet weak var myTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
