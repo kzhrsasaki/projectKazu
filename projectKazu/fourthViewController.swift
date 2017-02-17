33ea22443f6a0ec45f67e84d9d33aace02515289
kzhrsasaki <kzhr.sasaki@gmail.com>
2017-02-17 17:23:13 +0900

    2/17pm4:30現在

--- projectKazu/fourthViewController.swift
+++ projectKazu/fourthViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class fourthViewController: UIViewController {
 
--- projectKazu/secondViewController.swift
+++ projectKazu/secondViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class secondViewController: UIViewController, UITextFieldDelegate,UITextViewDelegate {
 
